42dcb0dc58dc2b1deba6914968f8c971e80a2a5e
Champ Phattarapon <s7070086@kmitl.ac.th>
2019-03-12 15:57:38 +0700

    first commit

---
+++ src/store/store_n10000.js
@@ -0,0 +1,33 @@
+let Web3 = require("web3");
+let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+let account = web3.eth.accounts[0];
+const fs = require("fs");
+
+///  Config. ////
+let n = 10000;
+let rate = 10 //  X transactions/sec.
+//// ------ ////
+
+let last_count = 0;
+let tx_list = [];
+console.log(`using account: ${account}`);
+let start_time = new Date();
+let loop_start_time = start_time;
+for (let i = 1 ; i<=n ; i++){
+    let tx_id = web3.eth.sendTransaction({from:account, data:"0x00"});
+    console.log(`i: ${i}, tx_id: ${tx_id}`);
+    tx_list.push(tx_id);
+    if(i - last_count == rate && new Date() - loop_start_time < 1000){
+        console.log(`waiting for 1 sec.`)
+        while(new Date() - loop_start_time < 1000){}
+        loop_start_time = new Date();
+        last_count = i;
+    }
+}
+
+let stop_time = new Date();
+console.log(`start time: ${start_time}\nstop time: ${stop_time}\nlap time: ${stop_time-start_time} ms.`);
+
+//console.log(tx_list)
+let json = JSON.stringify(tx_list);
+fs.writeFile('../n10000_tx_list', json, 'utf8', function(err){console.log(err)});
